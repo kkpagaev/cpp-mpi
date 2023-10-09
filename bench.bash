ae92e1c52739d3ed1862c049723b7eae342c9931
kkpagaev <kkpagaev@gmail.com>
2023-10-09 10:32:41 +0300

    c

--- bench.bash
+++ bench.bash
@@ -4,14 +4,14 @@
 g++ -fopenmp -o omp omp.cpp
 g++ -fopenmp -o default default.cpp
 # default
-for elements in 100000 1000000 2000000; do
-  hyperfine --runs 100 --warmup 12 "./default $elements" --shell=none --export-json "./results/default/default-elements-$elements.json"
-done
+# for elements in 100000 1000000 2000000; do
+#   hyperfine --runs 10 --warmup 1 "./default $elements" --shell=none --export-json "./results/default/default-elements-$elements.json"
+# done
 
 # openmp
 for threads in  1 2 3 4 5 6 7 8; do
   for elements in 100000 1000000 2000000; do
-    hyperfine --runs 100 --warmup 12 "./omp $elements $threads" --shell=none --export-json "./results/openmp-threads-$threads-elements-$elements.json"
+    hyperfine --runs 10 --warmup 1 "./omp $elements $threads" --shell=none --export-json "./results/$elements/openmp-threads-$threads-elements-$elements.json"
   done
 done
 
--- omp.cpp
+++ omp.cpp
@@ -6,43 +6,64 @@
 using namespace std;
 
 
-vector<double> vector_flater_omp(const vector<double> vec,  const vector<int> fixed_indexes) {
-  vector<double> flat(vec.size() - fixed_indexes.size());
-  size_t fixed_index = 0;
-  size_t i = 1;
+void vector_flater_omp(const vector<double>* vec, const vector<int>* fixed_indexes, vector<double>* flat) {
+  #pragma omp parallel for
+  for (size_t i = 1; i < vec->size() - 1; i++) {
+    bool skip = false;
+    for (size_t j = 0; j < fixed_indexes->size(); j++) {
+      if (i == (*fixed_indexes)[j]) {
+        skip = true;
+        break;
+      }
+    }
 
-  #pragma omp parallel for private(fixed_index)
-  for (i = 1; i < vec.size() - 1; i++) {
-    if (fixed_index < fixed_indexes.size() && i == fixed_indexes.at(fixed_index)) {
-      fixed_index++;
-
-      continue;
-    } else {
-      double avg = (vec.at(i - 1) + vec.at(i + 1));
+    if (!skip) {
+      double avg = (vec->at(i - 1) + vec->at(i + 1));
 
       avg /= 2;
 
-      flat[i] = avg;
+      // std::cout << vec->at(i - 1) << " + " << vec->at(i + 1) <<" index" << i << " avg: " << avg << std::endl;
+      (*flat)[i] = avg;
     }
   }
-
-  return flat;
 }
+
 
 int main(int argc, char* argv[]) {
   if (argc != 3) {
-    std::cout << "Usage: " << argv[0] << " <number> <number>" << std::endl;
+    cout << "Usage: " << argv[0] << " <number> <number>" << endl;
     return 1;
   }
 
-  int count = std::atoi(argv[1]);
-  int threads = std::atoi(argv[2]);
+  int count = atoi(argv[1]);
+  int threads = atoi(argv[2]);
 
   omp_set_num_threads(threads);
 
-  vector<double> v1    = vector<double>(count);
-  vector<int>    fixed = {1,3};
-  vector<double> flat  = vector_flater_omp(v1, fixed);
+  vector<double>* v1 = new vector<double>(count);
+  vector<int>* fixed = new vector<int>(2);
+  (*fixed)[0] = 1;
+  (*fixed)[1] = count - 2;
+
+  // cout << fixed;
+
+  vector<double>* flat = new vector<double>(count);
+  (*flat)[0] = 101;
+  (*flat)[1] = 100;
+  // (*flat)[count -11] = 100;
+  (*flat)[count - 2] = 100;
+
+
+  for (int i = 0; i < 1000; i++) {
+    vector_flater_omp(v1, fixed, flat);
+    auto temp = &v1;
+    v1 = flat;
+    flat = *temp;
+  }
+
+  // for (double f : *flat) {
+  //   cout << f << '\n';
+  // }
 
   return 0;
 }
